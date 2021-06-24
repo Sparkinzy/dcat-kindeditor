1105b86aa12253837e222bef50d6a0ae44b12b97
sparkinzy <sparkinzy@163.com>
2021-06-24 10:03:35 +0800

    去掉版本号前面的V

--- src/Setting.php
+++ src/Setting.php
@@ -8,7 +8,6 @@ class Setting extends Form
 {
     public function form()
     {
-        $this->text('key1')->required();
-        $this->text('key2')->required();
+        $this->text('readme')->required();
     }
 }
